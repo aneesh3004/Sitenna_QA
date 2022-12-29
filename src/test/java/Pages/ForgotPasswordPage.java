b5dd3e30dc0bdc2ba691cc845d70c2fde1c5b787
aneeshvyas <vyas.aneesh@gmail.com>
2022-12-29 17:26:39 +0530

    Forgot Password

---
+++ src/test/java/Pages/ForgotPasswordPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+import org.openqa.selenium.By;
+
+public class ForgotPasswordPage {
+
+	By forgotpassword_link = By.id("forgot_password");
+	By resetpassword_email = By.id("reset_password_email");
+	By resetpassword_btn = By.id("reset_password_button");
+	By create_new_password = By.id("new_password");
+	By repeat_new_password = By.id("repeat_new_password");
+	By continue_btn = By.id("create_password_continue_button");
+
+}
